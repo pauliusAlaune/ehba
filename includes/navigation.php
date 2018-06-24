ea1808e3150fb034fa31b01dc0ea57be76e7d4c9
pauliusAlaune <paulius.alaune@gmail.com>
2018-06-24 18:44:06 +0300

    working on header

---
+++ includes/navigation.php
@@ -0,0 +1,15 @@
+<header class="header">
+    <div class="header__holder">
+        <div class="header__holder__leftBlock">
+            <ul class="header__holder__leftBlock__listholder">
+                <li id="hoverItemList" class="header__holder__leftBlock__listholder--item hoverItem"><a href="">ABOUT</a></li>
+                <li class="header__holder__leftBlock__listholder--item hoverItem"><a href="">NEWS</a></li>
+                <li class="header__holder__leftBlock__listholder--item hoverItem"><a href="">EVENTS</a></li>
+            </ul>
+        </div>
+        <div class="header__holder__centerBlock">
+            <a href="#" class="header__holder__centerBlock--logo"><img src="assets/images/logo.svg"></a>
+        </div>
+        <div class="header__holder__rightBlock"></div>
+    </div>
+</header>
