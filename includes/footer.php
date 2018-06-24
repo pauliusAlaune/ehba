7d7925acec8df3a9ae04cfd7259786aeec8f7156
pauliusAlaune <paulius.alaune@gmail.com>
2018-06-24 14:36:57 +0300

    working on footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,35 @@
+    <footer class="footer">
+        <div class="topFooter">
+            <div class="topFooter__menuBlock">
+                <ul class="topFooter__menuBlock__listHolder">
+                    <li class="topFooter__menuBlock__listHolder--item"><a href="#">About us</a></li>
+                    <li class="topFooter__menuBlock__listHolder--item"><a href="#">Blog</a></li>
+                    <li class="topFooter__menuBlock__listHolder--item"><a href="#">Store</a></li>
+                    <li class="topFooter__menuBlock__listHolder--item"><a href="#">Events</a></li>
+                </ul>
+            </div>
+            <div class="topFooter__socialBlock">
+                <a href="#" class="topFooter__socialBlock--link"><img src=""></a>
+            </div>
+            <div class="topFooter__getNewsBlock">
+                <p class="topFooter__getNewsBlock--name">Get latest news</p>
+                <form class="getLastestNews" id="getLastestNews">
+                    <div class="getLastestNews__holder">
+                        <input type="text" name="email" id="getLatestNewsEmail" class="getLastestNews__holder--email" placeholder="Your email here">
+                        <input type="submit" value="Subscribe" id="getLatestNewsSubmit" class="getLastestNews__holder--submit">
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="bottomFooter">
+            <div class="bottomFooter__leftBlock">
+                <p class="bottomFooter__leftBlock--copyRight">© 2018 EHBA</p>
+            </div>
+            <div class="bottomFooter__rightBlock">
+                <p class="bottomFooter__rightBlock--madeBy">made by <a href="#" title=">mancanweb">mancanweb</a></p>
+            </div>
+        </div>
+    </footer>
     <script src="assets/js/jquery-3.2.1.min.js"></script>
     <script src="assets/js/app.js"></script>
 </body>
