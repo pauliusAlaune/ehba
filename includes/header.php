7136b17354bbce26ccaedee046d3c63af5872521
pauliusAlaune <paulius.alaune@gmail.com>
2018-06-24 13:13:48 +0300

    preparatory work

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,900&amp;subset=latin-ext" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/reset.css">	
+  <link rel="stylesheet" href="assets/css/style.css">
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <title>EHBA</title>
+</head>
+<body>
+
+<?php include './includes/navigation.php'; ?>
+
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include './includes/header.php'; ?>
+
+<h1 class="testTitle">What is EHBA 
+and Bike Polo</h1>
+
+<?php include './includes/footer.php'; ?>
