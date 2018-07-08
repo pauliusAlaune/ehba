e72a9536efa88507ff4e5e801c1bba8bac327ed0
pauliusAlaune <paulius.alaune@gmail.com>
2018-07-08 21:41:18 +0300

    index page desktop version

--- index.php
+++ index.php
@@ -10,4 +10,177 @@
     </div>
 </section>
 
+<section class="introNewsSection">
+    <div class="introNewsSection__titleHolder">
+        <h3 class="introNewsSection__titleHolder--mainTitle">News</h3>
+        <h5 class="introNewsSection__titleHolder--subTitle">LATEST UPDATES</h5>
+    </div>
+    <div class="introNewsSection__holder">
+        <div class="introNewsSection__holder__leftBlock">
+            <a href="" class="introNewsSection__holder__leftBlock--link">
+                <div class="leftBlockLink__block">
+                    <div class="leftBlockLink__block--imgBlock">
+                        <img src="assets/images/introNews.jpg">
+                        <span class="overlay"></span>
+                    </div>
+                    <div class="leftBlockLink__block--textBlock">
+                        <p class="title">An Ugly Myspace Profile Will Sure Ruin Your Reputation</p>
+                        <p class="para">Compatible Inkjet Cartridge Which One Will You Choose</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="introNewsSection__holder__rightBlock">
+            <div class="introNewsSection__holder__rightBlock__twoBlockHolder">
+                <div class="introNewsSection__holder__rightBlock__twoBlockHolder__block">
+                    <a href="" class="introNewsSection__holder__rightBlock__twoBlockHolder__block--link">
+                        <div class="rightBlockLink__block">
+                            <div class="rightBlockLink__block--imgBlock">
+                                <img src="assets/images/introNews2.jpg" alt="">
+                                <span class="overlay"></span>
+                            </div>
+                            <div class="rightBlockLink__block--textBlock">
+                                <p class="title">Why Las Vegas Hotel Rooms For You</p>
+                                <p class="para">Compatible Inkjet Cartridge Which One Will You Choose</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="introNewsSection__holder__rightBlock__twoBlockHolder__block">
+                    <a href="" class="introNewsSection__holder__rightBlock__twoBlockHolder__block--link">
+                        <div class="rightBlockLink__block">
+                            <div class="rightBlockLink__block--imgBlock">
+                                <img src="assets/images/introNews3.jpg" alt="">
+                                <span class="overlay"></span>
+                            </div>
+                            <div class="rightBlockLink__block--textBlock">
+                                <p class="title">An Ugly Myspace Profile Will Sure Ruin Your Reputation</p>
+                                <p class="para">Compatible Inkjet Cartridge Which One Will You Choose</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="introNewsSection__linkHolder">
+        <a href="" class="introNewsSection__linkHolder--link">Read all</a>
+    </div>
+</section>
+
+<section class="introEventsSection">
+    <div class="introEventsSection__titleHolder">
+        <h3 class="introEventsSection__titleHolder--mainTitle">Events</h3>
+        <h5 class="introEventsSection__titleHolder--subTitle">FEATURED BIKE POLO EVENTS</h5>
+    </div>
+    <div class="introEventsSection__holder">
+        <a href="" class="introEventsSection__holder__block">
+            <div class="introEventsSection__holder__block__inner">
+                <div class="introEventsSection__holder__block__inner__leftBlock">
+                    <img src="assets/images/introEvents1.jpg" alt="">
+                    <span class="overlay"></span>
+                    <span class="hoverBorder"></span>
+                </div>
+                <div class="introEventsSection__holder__block__inner__rightBlock">
+                    <div class="nameDateBlock">
+                        <p class="title">Indian Summer Bike Polo Games Vol.3</p>
+                        <p class="date">27 March 2018</p>
+                    </div>
+                    <div class="placeBlock">
+                        <p class="place"><img src="assets/images/location-pin-marker-gps-map-optimization-place.svg"><span class="name">Vilnius</span></p>
+                        <p class="sort"><img src="assets/images/mixed.svg"><span class="name">Mixed</span></p>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <a href="" class="introEventsSection__holder__block">
+            <div class="introEventsSection__holder__block__inner">
+                <div class="introEventsSection__holder__block__inner__leftBlock">
+                    <img src="assets/images/introEvents1.jpg" alt="">
+                    <span class="overlay"></span>
+                    <span class="hoverBorder"></span>
+                </div>
+                <div class="introEventsSection__holder__block__inner__rightBlock">
+                    <div class="nameDateBlock">
+                        <p class="title">Indian Summer Bike Polo Games Vol.3</p>
+                        <p class="date">27 March 2018</p>
+                    </div>
+                    <div class="placeBlock">
+                        <p class="place"><img src="assets/images/location-pin-marker-gps-map-optimization-place.svg"><span class="name">Vilnius</span></p>
+                        <p class="sort"><img src="assets/images/mixed.svg"><span class="name">Mixed</span></p>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <a href="" class="introEventsSection__holder__block">
+            <div class="introEventsSection__holder__block__inner">
+                <div class="introEventsSection__holder__block__inner__leftBlock">
+                    <img src="assets/images/introEvents1.jpg" alt="">
+                    <span class="overlay"></span>
+                    <span class="hoverBorder"></span>
+                </div>
+                <div class="introEventsSection__holder__block__inner__rightBlock">
+                    <div class="nameDateBlock">
+                        <p class="title">Indian Summer Bike Polo Games Vol.3</p>
+                        <p class="date">27 March 2018</p>
+                    </div>
+                    <div class="placeBlock">
+                        <p class="place"><img src="assets/images/location-pin-marker-gps-map-optimization-place.svg"><span class="name">Vilnius</span></p>
+                        <p class="sort"><img src="assets/images/mixed.svg"><span class="name">Mixed</span></p>
+                    </div>
+                </div>
+            </div>
+        </a>
+        <a href="" class="introEventsSection__holder__block">
+            <div class="introEventsSection__holder__block__inner">
+                <div class="introEventsSection__holder__block__inner__leftBlock">
+                    <img src="assets/images/introEvents1.jpg" alt="">
+                    <span class="overlay"></span>
+                    <span class="hoverBorder"></span>
+                </div>
+                <div class="introEventsSection__holder__block__inner__rightBlock">
+                    <div class="nameDateBlock">
+                        <p class="title">Indian Summer Bike Polo Games Vol.3</p>
+                        <p class="date">27 March 2018</p>
+                    </div>
+                    <div class="placeBlock">
+                        <p class="place"><img src="assets/images/location-pin-marker-gps-map-optimization-place.svg"><span class="name">Vilnius</span></p>
+                        <p class="sort"><img src="assets/images/mixed.svg"><span class="name">Mixed</span></p>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="introEventsSection__linkHolder">
+        <a href="" class="introEventsSection__linkHolder--link">See all</a>
+        <a href="" class="introEventsSection__linkHolder--link">Submit event</a>
+    </div>
+</section>
+
+<section class="introWhatIsSection">
+    <div class="introWhatIsSection__holder">
+        <div class="introWhatIsSection__holder__leftBlock">
+            <img src="assets/images/introWhatIs.jpg">
+        </div>
+        <div class="introWhatIsSection__holder__rightBlock">
+            <h3 class="title">What is Bike Polo and EHBA</h3>
+            <div class="deco"></div>
+            <p class="para">Buying the right telescope to take your love of astronomy to the next level is a big next step in the development of your passion for the stars. In many ways, it is a big step from someone who is just fooling around with astronomy to a serious student of the science. But you and I both know that there is still another big step after buying a telescope before you really know how to use it.</p>
+            <a href="" class="link">Read all</a>
+        </div>
+    </div>
+</section>
+
+<section class="introThanksSection">
+    <div class="introThanksSection__titleHolder">
+        <h3 class="introThanksSection__titleHolder--mainTitle">Special thanks to:</h3>
+    </div>
+    <div class="introThanksSection__holder">
+        <a href="" class="link">Legal Guns</a>
+        <a href="" class="link">mancanweb</a>
+        <a href="" class="link">mountainbike</a>
+        <a href="" class="link">Philip Morris</a>
+    </div>
+</section>
+
 <?php include './includes/footer.php'; ?>
