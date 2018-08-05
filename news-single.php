f82ba3d299865db364ccd95f4463c96567647730
pauliusAlaune <paulius.alaune@gmail.com>
2018-08-05 22:34:07 +0300

    working with single news page

---
+++ news-single.php
@@ -0,0 +1,59 @@
+<?php include './includes/header.php'; ?>
+
+<section class="newsSingleSection">
+    <div class="newsSingleSection__holder">
+        <div class="newsSingleSection__holder__catBlock">
+            <ul class="catListHolder">
+                <li class="catListHolder__item active">All posts</li>
+                <li class="catListHolder__item">Rule Set Changes</li>
+                <li class="catListHolder__item dropClick">
+                    <span class="dropdown">
+                        <span class="dropdown--text">Community</span>
+                        <span class="dropdown--icon"><img src="assets/images/ico-dropdown.svg"></span>
+                    </span>
+                    <ul class="dropdown__list">
+                        <li class="dropdown__list--item">Radio astronomy</li>
+                        <li class="dropdown__list--item">A  beginners guide</li>
+                    </ul>
+                </li>
+                <li class="catListHolder__item">Radio astronomy</li>
+                <li class="catListHolder__item dropClick">
+                    <span class="dropdown">
+                        <span class="dropdown--text">A  beginners guide</span>
+                        <span class="dropdown--icon"><img src="assets/images/ico-dropdown.svg"></span>
+                    </span>
+                    <ul class="dropdown__list">
+                        <li class="dropdown__list--item">Radio astronomy</li>
+                        <li class="dropdown__list--item">A  beginners guide</li>
+                    </ul>
+                </li>
+                <li class="catListHolder__item dropClick">
+                    <span class="dropdown">
+                        <span class="dropdown--text">How to play Bike Polo</span>
+                        <span class="dropdown--icon"><img src="assets/images/ico-dropdown.svg"></span>
+                    </span>
+                    <ul class="dropdown__list">
+                        <li class="dropdown__list--item">Radio astronomy</li>
+                        <li class="dropdown__list--item">A  beginners guide</li>
+                    </ul>
+                </li>
+                <li class="catListHolder__item">What is homeopatic medicine</li>
+            </ul>
+        </div>
+        <div class="newsSingleSection__holder__postsBlock">
+            <a href="" class="backBtn">
+                <img class="backBtn--icon" src="assets/images/ico-dropdown.svg">
+                <span class="backBtn--name">Back</span>
+            </a>
+            <div class="singlePostBlock">
+                <h1 class="title">7 Ways To Advertise Your Business For Free</h1>
+                <div class="imgHolder">
+                    <div class="imgHolder--img" style="background-image:url('assets/images/rectangle-3.jpg');"></div>                    
+                </div>
+                <p class="para">Many conventional colleges and universities are now offering online DVD repair courses, which are the exact same as those taught on a traditional college campus, to distance learners. Online education is generally taught through the use of several different methods, including online satellite feeds, correspondence or online lessons. This technique permits the student to obtain a DVD repair degree without ever having to step inside of a classroom. An online education, providing that it is granted by a regionally accredited college or university, is broadly accepted as worthy training for any job in the direct field of study.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include './includes/footer.php'; ?>
