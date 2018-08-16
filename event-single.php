58707d1a2a9d4ed1afc007477e76f37f3b615278
pauliusAlaune <paulius.alaune@gmail.com>
2018-08-16 21:11:45 +0300

    almost finish event single page

--- event-single.php
+++ event-single.php
@@ -1,22 +1,75 @@
 <?php include './includes/header.php'; ?>
 
 <section class="eventSingleSection">
-	<div class="eventSingleSection__holder">
-		<a href="" class="backBtn">
-        	<img class="backBtn--icon" src="assets/images/ico-dropdown.svg">
-            <span class="backBtn--name">Back</span>
-    	</a>
-    	<div class="eventPrevNextHolder">
+	<a href="" class="backBtn">
+       	<img class="backBtn--icon" src="assets/images/ico-dropdown.svg">
+        <span class="backBtn--name">Back</span>
+    </a>
+    <div class="eventSingleSection__holder">
+    	<div class="eventSingleSection__holder__introWrapper">
+    		<div class="eventSingleSection__holder__introWrapper__leftBlock">
+    			<div class="eventSingleSection__holder__introWrapper__leftBlock__imgHolder">
+    				<div class="eventSingleSection__holder__introWrapper__leftBlock__imgHolder--img"></div>
+    			</div>
+    		</div>
+    		<div class="eventSingleSection__holder__introWrapper__rightBlock">
+    			<h1 class="eventName">NIDA BEACH POLO CAMP 2018</h1>
+					<p class="date">March 27-28</p>
+					<div class="registrationAndPriceBlock">
+						<button class="registrationBtn">Registration</button>
+						<p class="price">$1500</p>
+					</div>
+    		</div>
+    		<div class="eventSingleSection__holder__introWrapper__leftBlock">
+    			<p class="para">Motivation can be simply defined as the things that induce us to take certain actions or behave in a certain way. It is the why or the reason that drives a person towards a desired goal.</p>
+					<p class="para">The reasons can be categorized into something that is within us i.e. intrinsic / internal motivation, or something that is external to us i.e. extrinsic / external motivation.Internal motivation in performing a task occurs when the task in itself is experienced as rewarding and there is no need for any external reward before it is performed. Examples are love of the task, self-satisfaction, sense of achievement, relaxation purpose, etc</p>
+    		</div>
+				<div class="eventSingleSection__holder__introWrapper__rightBlock">
+					<div class="moreInfo">
+						<h6 class="title">DATE AND TIME</h6>
+						<p class="para">March 27-28</p>
+						<p class="para">10:00 - 17:00</p>
+						<a class="link" href="">Add to calendar</a>
+					</div>
+					<div class="moreInfo">
+						<h6 class="title">LOCATION</h6>
+						<p class="para">Jordan Park (Tennis Courts)</p>
+						<p class="para">1060 S 900 W</p>
+						<p class="para">Salt Lake City, UT 84104-1515</p>
+						<p class="para">United States</p>
+						<a class="link" href="">View map</a>
+					</div>
+					<div class="moreInfo">
+						<h6 class="title">ORGANIZER</h6>
+						<p class="para">Beehive Bike Polo</p>
+						<p class="para"><a href="tel:3852888681">3852888681</a></p>
+						<p class="para"><a href="mailto:david:barthod@gmail.com">david:barthod@gmail.com</a></p>
+					</div>
+				</div>
+    	</div>
+    </div>
+    <div class="eventPrevNextHolder">
 			<a href="" class="prevBtn">
 				<img class="prevBtn--icon" src="assets/images/ico-dropdown.svg">
 				<span class="prevBtn--name">Previous event</span>
 			</a>
-           	<a href="" class="nextBtn">
-           		<span class="nextBtn--name">Next event</span>
+			<a href="" class="nextBtn">
+				<span class="nextBtn--name">Next event</span>
 				<img class="nextBtn--icon" src="assets/images/ico-dropdown.svg">
 			</a>
+    </div>
+    <div class="specialThanksHolder">
+    	<h3 class="title">Special thanks to:</h3>
+    	<div class="thanksLink__holder">
+        <a href="" class="link">Legal Guns</a>
+        <a href="" class="link">mancanweb</a>
+        <a href="" class="link">mountainbike</a>
+        <a href="" class="link">Philip Morris</a>
     	</div>
-	</div>
+    </div>
+    <div class="eventMap__holder">
+    	<div class="eventMap__holder--map"></div>
+    </div>
 </section>
 
 <?php include './includes/footer.php'; ?>
