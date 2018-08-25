463972718b6d7fcd59125321cefc84f195f5bfad
pauliusAlaune <paulius.alaune@gmail.com>
2018-08-25 22:20:22 +0300

    complete guide-to-recruit-new-players psuedo support page for desktop

--- support.php
+++ support.php
@@ -4,9 +4,9 @@
     <div class="supportSection__holder">
         <div class="supportSection__holder__navbarBlock">
             <ul id="supportNavbar" class="navbarListHolder">
-                <li id="ehbc" class="navbarListHolder__item active">EHBC</li>
+                <li id="ehbc" class="navbarListHolder__item">EHBC</li>
                 <li id="donate" class="navbarListHolder__item">Donate</li>
-                <li id="drills" class="navbarListHolder__item">Drills</li>
+                <li id="drills" class="navbarListHolder__item active">Drills</li>
                 <li id="guide-to-recruit-new-players" class="navbarListHolder__item">Guide to recruit new players</li>
                 <li id="working-with-schools" class="navbarListHolder__item">Working with schools</li>
                 <li id="official-help-for-clubs" class="navbarListHolder__item">Official help for clubs</li>
--- supportInnerContent/guide-to-recruit-new-players.php
+++ supportInnerContent/guide-to-recruit-new-players.php
@@ -1,9 +1,9 @@
 <div class="fade">
     <div class="guideHolder">
         <div class="guideHolder__block">
-            <div class="deco"></div>
+            <div class="introDeco"></div>
             <span class="blockNumber">01</span>
-            <div class="guideHolder__block__titleHolder">
+            <div class="titleHolder">
                 <h3 class="title">REQUIREMENTS</h3>
             </div>
             <div class="textblock">
@@ -16,7 +16,46 @@
             </div>
         </div>
         <div class="guideHolder__block">
-            <div class="doubleBlock"></div>
+            <div class="doubleBlock">
+                <div class="doubleBlock__inner">
+                    <div class="titleHolder">
+                        <h3 class="title">WHAT TO EXPECT</h3>
+                    </div>
+                    <span class="blockNumber moreRight">02</span>
+                    <div class="doubleBlock__inner__textblock padLeft">
+                        <div class="deco"></div>
+                        <p class="para">Airport hotels may not enjoy the best of reputations. In many peoples mind they are ranked alongside train station pubs – filled with chain smoking business men in cheap suits. However, while there is certainly a convincing argument to avoid train station pubs (you have to pay to get in the toilet for starters), airport hotels are a long way from their cheap and not-so-cheerful roots. These days the savvy traveller can pick up a great value deal at some excellent hotel accommodation situated close to their airport of departure.</p>
+                    </div>
+                </div>
+                <div class="doubleBlock__inner">
+                    <div class="doubleBlock__inner__imgBlock">
+                        <span class="doubleBlock__inner__imgBlock--imgBlockNumber">02</span>
+                        <div class="doubleBlock__inner__imgBlock--img" style="background-image: url('./assets/images/guide2.jpg')"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="guideHolder__block">
+            <div class="doubleBlock">
+                <div class="doubleBlock__inner">
+                    <div class="doubleBlock__inner__imgBlock">
+                        <span class="doubleBlock__inner__imgBlock--imgBlockNumber rightSide">03</span>
+                        <div class="doubleBlock__inner__imgBlock--img" style="background-image: url('./assets/images/guide3.jpg')"></div>
+                    </div>
+                </div>
+                <div class="doubleBlock__inner">
+                    <div class="titleHolder">
+                        <h3 class="title">THE ROLE</h3>
+                    </div>
+                    <span class="blockNumber">03</span>
+                    <div class="doubleBlock__inner__textblock padRightPx">
+                       <div class="deco zeroLeft"></div>
+                        <p class="para">1. Airport hotels may not enjoy the best of reputations. In many peoples mind they are ranked alongside train station pubs – filled with chain smoking business men in cheap suits. However, while there is certainly a convincing argument to avoid train station pubs (you have to pay to get in the toilet for starters), airport hotels are a long way from their cheap and not-so-cheerful.</p>
+                        <p class="para">2. These days the savvy traveller can pick up a great value deal at some excellent hotel accommodation situated close to their airport of departure.</p>
+                        <p class="para">3. One hotel that certainly fits this bill is the 4-star Arora International Hotel near Gatwick (http://www.gatwickairport.com). Situated a mere 8 minutes from the airport, the hotel boasts a health club, with state of the art facilities including a gym, jacuzzi, steam room and solarium. </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
