d0c609c25971a751ef516ef5d095adb069ff4e80
pauliusAlaune <paulius.alaune@gmail.com>
2018-08-25 16:48:36 +0300

    work with guide-to-recruit-new-players support page

---
+++ supportInnerContent/guide-to-recruit-new-players.php
@@ -0,0 +1,22 @@
+<div class="fade">
+    <div class="guideHolder">
+        <div class="guideHolder__block">
+            <div class="deco"></div>
+            <span class="blockNumber">01</span>
+            <div class="guideHolder__block__titleHolder">
+                <h3 class="title">REQUIREMENTS</h3>
+            </div>
+            <div class="textblock">
+                <p class="para">Unless you have the dubious pleasure of living right next door to an airport one of the biggest downsides of going on holiday is catching a flight at an ungodly hour of the day. </p>
+                <p class="para">While driving half asleep at 4.00 am, with a nagging doubt you have forgot something may be some holidaymaker’s idea of fun – for most it sounds like a bad way to start your holiday. However, help is at hand in the form of Airport Hotels, where you can spend a relaxed evening – and even afford an extra hour or two in bed before leaving.</p>
+            </div>
+            <div class="imgBlock">
+                <span class="imgBlockNumber">01</span>
+                <div class="imgBlock--img" style="background-image: url('./assets/images/guide1.jpg')"></div>
+            </div>
+        </div>
+        <div class="guideHolder__block">
+            <div class="doubleBlock"></div>
+        </div>
+    </div>
+</div>
